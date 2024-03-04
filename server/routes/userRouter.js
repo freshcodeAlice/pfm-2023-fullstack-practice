8a05bb31c0a4e5c75f61cb5c822ef354fccc62e8
freshcodeAlice <freshjs.ofte@gmail.com>
2024-03-04 12:02:06 +0200

    Auth algorytm

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,6 +2,7 @@ const userRouter = require('express').Router();
 const UserController = require('../controllers/User.controller');
 const {hashPass} = require('../middlewares/hashPass');
 
-userRouter.post('/', hashPass, UserController.createUser);
+userRouter.post('/sign-up', hashPass, UserController.signUp);
+userRouter.post('/sign-in', UserController.signIn);
 
 module.exports = userRouter;
