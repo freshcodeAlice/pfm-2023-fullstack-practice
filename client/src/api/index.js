fe5895d7a920dbd2a504d987d153742c93ec1d15
freshcodeAlice <freshjs.ofte@gmail.com>
2024-03-14 11:10:55 +0200

    axios interseptors done

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import history from '../history';
 
 const httpClient = axios.create({
     baseURL: 'http://localhost:5000/api'
@@ -19,6 +20,41 @@ httpClient.interceptors.request.use((config) => {
 (err) => { Promise.reject(err)});
 
 
+httpClient.interceptors.response.use((response)=> {
+    // success handler
+    // Виконується, якщо у відповіді 1хх, 2хх, 3хх статус-код
+    if (response.data.tokens) {
+        const {data: {tokens: {accessToken, refreshToken}}} = response
+    // якщо в успішній відповіді прийшли токени - маємо їх покласти до localStorage
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+    }
+    return response
+}, (error)=>{
+    // error handler
+
+    /*
+    Якщо помилка з кодом 403 - токен коцнутий(або прострочився). Необхний рефреш сесії
+    Якщо помилка з кодом 401 - аксессТокен відсутній або рефреш не вдався, необхідно перелогінити юзера
+    */
+    if (error.response.status === 403 ) {
+        // рефрешримо сесію 
+        // маємо зробити запит на /refresh з РТ, щоби оновити сесію, а після цього повторно зробити початковий запит за ресурсом, який хотів юзер
+        refreshSession()
+        .then(() => {
+            // коли запит на оновлення сесії успішно повернувся і поклав до LS свіжі токени - робимо заново той же самий запит
+            return httpClient(error.config); 
+        })
+
+    } else if (error.response.status === 401) {
+        logOut();
+        /// перекидаємо юзера на сторінку авторизації
+        history.replace('/');
+    }
+
+})
+
+
 /// TODO: response interceptors
 // Якщо приходить 403 помилка - намагаємося оновити сесію за допомогою RefreshToken
 
@@ -26,6 +62,17 @@ httpClient.interceptors.request.use((config) => {
 
 export const signIn = async (data) => await httpClient.post('/users/sign-in', data);
 export const signUp = async (data) => await httpClient.post('/users/sign-up', data);
+
+
+export const refreshSession = async () => {
+    // беремо з localStorage refreshToken і надсилаємо його на /refresh
+    const rt = localStorage.getItem('refreshToken');
+    return await httpClient.post('/users/refresh', {rt});
+}
+
+export const logOut = async () => {
+    localStorage.clear();
+}
 
 
 /* Chat API */
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -15,8 +15,7 @@ const Home = (props) => {
     const sendData = (data) => {
         console.log(data);
         props.setUser(data);
-        localStorage.setItem('accessToken', data.tokens.accessToken);
-        localStorage.setItem('refreshToken', data.tokens.refreshToken);
+
         // після оновлення даних у стейті App - хочемо автоматично перенаправити користувача на дашборд
         history.push('/messenger')
     }
