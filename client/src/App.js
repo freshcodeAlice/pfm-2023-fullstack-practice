066d8f789cc439590136ced20bcd3ee1f361bfa5
freshcodeAlice <freshjs.ofte@gmail.com>
2024-03-19 12:02:33 +0200

    app need refactor!

--- client/src/App.js
+++ client/src/App.js
@@ -5,34 +5,57 @@ import {unstable_HistoryRouter as Router, Routes, Route} from 'react-router-dom'
 // import UserContext from './contexts/UserContext';
  import history from './history';
 import './reset.css';
+import {connect} from 'react-redux';
 import { getUserData } from './api';
 
-function App() {
-  // const [user, setUser] = useState(null);
+function App(props) {
 
-  // useEffect(() => {
 
-  //   if (!user) {
-  //     // ідемо за юзером.
-  //     // якщо отримуємо її - кладемо у стейт    
-  //     console.log('getUserData')
-  //     getUserData().then(({data: {data}}) => {
-  //       setUser(data)
-  //     })
-  //   }   
-  // }, []);
+  useEffect(() => {
+
+    if (!props.user && localStorage.getItem('accessToken')) {
+      // ідемо за юзером.
+      // якщо отримуємо її - кладемо у стейт    
+      console.log('getUserData')
+      getUserData()
+      .then(({data: {data}}) => {
+          // у нас є в пропсах функція dispatch
+          const action = {
+            type: 'GET_USER_DATA',
+            payload: data
+          }
+          props.dispatch(action)
+      })
+      .catch(error => {
+        const action = {
+          type: 'USER_DATA_ERROR_FETCHING',
+          error
+        }
+        props.dispatch(action)
+      })
+    }   
+  }, []);
 
 
   return (
- 
+    <>
    <Router history={history}>
       <Routes>
         <Route path='/' exact element={<Home/>} />
         {/* <Route path='/messenger' element={<Dashboard />} /> */}
       </Routes>
     </Router>
+    {props.error && <p>Ooops, something goes wrong</p>}
+    </>
   );
 }
 
-export default App;
+const mapStateToProps = ({user, error}) => ({user, error})
+  // визначає, яка частина стора нам потрібна тут!
 
+
+export default connect(mapStateToProps)(App);
+
+
+
+/// NEED refactor: чому ми провалились в безкінечний цикл?
--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,15 +1,27 @@
 
 const initialStates = {
     user: null,
-    currentChat: null
+    currentChat: null,
+    error: null
   };
   
 
 function reducer (state = initialStates, action) {   // Pure function!
     console.log(action);
-    // switch(action.type) {
-     
-    // }
+    switch(action.type) {
+        case 'GET_USER_DATA': {
+            return {
+                ...state,
+                user: action.payload
+            }
+        }
+        case 'USER_DATA_ERROR_FETCHING': {
+            return {
+                ...state,
+                error: action.error.message
+            }
+        }
+    }
     return state;
   }
   
