175d806923f3e1f1fc3d4032b57c89cf9b2f78bc
freshcodeAlice <freshjs.ofte@gmail.com>
2024-03-19 10:17:19 +0200

    Redux welcome

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,53 @@ import Dashboard from './pages/Dashboard';
 import {unstable_HistoryRouter as Router, Routes, Route} from 'react-router-dom';
 import UserContext from './contexts/UserContext';
  import history from './history';
- import MyComponent from './MyComponent'
+ import MyComponent from './MyComponent';
+ import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 import './reset.css';
 import { getUserData } from './api';
+
+const initialStates = {
+  count: 0,
+  step: 0,
+  anotherValue: 'super-impotant-value'
+};
+
+const ACTION_TYPES = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+  STEP_CHANGE: 'STEP_CHANGE'
+}
+
+const store = createStore(reducer);
+
+function reducer (state = initialStates, action) {   // Pure function!
+  console.log(action);
+  switch(action.type) {
+      case ACTION_TYPES.INCREMENT: {
+          return {
+              ...state,
+              count: state.count + 1
+          } 
+      }
+      case ACTION_TYPES.DECREMENT: {
+          return {
+              ...state,
+              count: state.count - 1
+          }
+      }
+      case ACTION_TYPES.STEP_CHANGE: {
+          return {
+              ...state,
+              step: Number(action.value)
+          }
+      }
+  }
+  return state;
+}
+
+
 
 function App() {
   // const [user, setUser] = useState(null);
@@ -26,8 +69,14 @@ function App() {
 
 
   return (
-    <MyComponent />
-    // <Router history={history}>
+    <Provider store={store}>
+    {/* Provider має огортати весь додаток, щоб роздавати значення стора нижче по дереву компонент */}
+      <MyComponent />
+    </Provider>
+   
+   
+   
+   // <Router history={history}>
     //   <UserContext.Provider value={user}>
     //   <Routes>
     //     <Route path='/' exact element={<Home setUser={setUser}/>} />
--- client/src/MyComponent.js
+++ client/src/MyComponent.js
@@ -1,70 +1,72 @@
 import React, {useState, useReducer} from 'react';
+import {connect} from 'react-redux';
 
 
-function reducer (state, action) {
-    switch(action.type) {
-        case 'INCREMENT': {
-            return {
-                ...state,
-                count: state.count + state.step
-            } 
-        }
-        case 'DECREMENT': {
-            return {
-                ...state,
-                count: state.count - state.step
-            }
-        }
-        case 'STEP_CHANGE': {
-            return {
-                ...state,
-                step: Number(action.value)
-            }
-        }
-    }
-    return state;
+
+const ACTION_TYPES = {
+    INCREMENT: 'INCREMENT',
+    DECREMENT: 'DECREMENT',
+    STEP_CHANGE: 'STEP_CHANGE'
 }
 
+
+
 const MyComponent = (props) => {
-    const [state, dispatch] = useReducer(reducer, {
-                                        count: 0,
-                                        step: 0
-                                            });
+
+    console.log(props);
+    
+    // const [state, dispatch] = useReducer(reducer, {
+    //                                     count: 0,
+    //                                     step: 0
+    //                                         });
     
     const increment = () => {
         // setCount(count + step)
-        dispatch({
-            type: 'INCREMENT'
+        props.dispatch({
+            type: ACTION_TYPES.INCREMENT
         })
     }
 
     const decrement = () => {
         // setCount(count - step)
-        dispatch({
-            type: 'DECREMENT'
+        props.dispatch({
+            type: ACTION_TYPES.DECREMENT
         })
     }
 
     const changeHandler = ({target: {value}}) => {
         // setStep(Number(value));
-        dispatch({
-            type: 'STEP_CHANGE',
-            value
-        })
-    }
+    //     props.dispatch({
+    //         type: ACTION_TYPES.STEP_CHANGE,
+    //         value
+    //     })
+     }
     
     return (
+       
         <div>
-            <h1>{state.count}</h1>
-            <input name="step" value={state.step} onChange={changeHandler} />
+            <h1>{props.count}</h1>
+            {/* <input name="step" value={state.step} onChange={changeHandler} /> */}
             <button onClick={increment}>+</button>
             <button onClick={decrement}>-</button>
         </div>
     );
 }
 
-export default MyComponent;
+//export default MyComponent;
 
+
+const mapStateToProps = ({count, step}) => {
+    // функція, яка приймає весь об'єкт стору і повертає тільки ту частину, яка потрібна поточній компоненті
+    // повернена частина стору вкладається компоненті у пропси!
+    return {count, step}
+}
+
+// const HOC = connect(mapStateToProps);
+// const wrappedComponent = HOC(MyComponent);
+
+
+export default connect(mapStateToProps)(MyComponent);
 
 
 /*
@@ -73,5 +75,36 @@ export default MyComponent;
 | 1. Локальний стан компоненти
 | 2. Контекст
 - 3. Redux
+
+*/
+
+
+
+
+/*
+REDUX
+
+Передбачуваний контейнер для стану
+
+UI за допомогою функції dispatch відправляє об'єкт action до функції-редьюсера
+Reducer приймає поточний state, action і на основі них оновлює store
+Оновлення store призводить до оновлення ПІДПИСАНИХ на цю частину стору компонент.
+
+
+*/
+
+/*
+
+Redux --- store
+
+React-redux --- підписка на store і оновлення потрібних компонент
+
+
+
+
++1. Створити та налаштувати store
++2. Створити функцію-редьюсер для керування store
++3. Створити та налаштувати Provider, який роздає значення store всім підключеним компонентам
+4. Підключити до провайдера компоненти (підписати їх на оновлення)
 
 */
